8fc202f688eba7e6b37310a95243471a24eb34f3
PowerfullUser <139379401+PowerfullUser@users.noreply.github.com>
2025-11-09 23:07:20 +0200

    Update main.py

--- Release 2/main.py
+++ Release 2/main.py
@@ -1,3 +1,19 @@
+# PowerBrowser - A multi-tabbed, rich-text editor built with PyQt6.
+#
+# Copyright 2025 Toshan Chowdhury
+#
+# Licensed under the Apache License, Version 2.0;
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from PyQt6.QtWidgets import *
 from PyQt6.QtWebEngineWidgets import *
 from PyQt6.QtCore import *
@@ -685,3 +701,4 @@ app.setApplicationVersion(version)
 window = MainWindow()
 window.showMaximized()
 app.exec()
+
