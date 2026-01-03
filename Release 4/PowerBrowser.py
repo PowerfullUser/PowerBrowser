d8b7ff834d8bb9f2dd6a8b6ed7bda635caaa5f39
TCH124 <139379401+PowerfullUser@users.noreply.github.com>
2026-01-03 11:01:56 +0200

    Update PowerBrowser.py

--- Release 4/PowerBrowser.py
+++ Release 4/PowerBrowser.py
@@ -1,3 +1,21 @@
+# PowerBrowser - A multi-tabbed, rich-text editor built with PyQt6.
+#
+# Copyright 2026 Toshan Chowdhury
+#
+# Licensed under the GNU General Public License, Version 3.0 (GPLv3);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.gnu.org/licenses/gpl-3.0.en.html
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see the link above.
+
 import os
 import sys
 from PyQt6.QtWidgets import *
@@ -548,4 +566,5 @@ window.setMinimumWidth(1000)
 window.setMinimumHeight(600)
 window.showMaximized()
 app.exec()
+
 
